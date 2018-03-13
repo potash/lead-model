eca0d23075a622629d9833572f21850235e9947c
Eric Potash <eric@k2co3.net>
2018-03-13 18:23:57 -0500

    predict on new addresses using geographies

--- lead/model/score.py
+++ lead/model/score.py
@@ -13,8 +13,20 @@ class LeadScore(Step):
             if y is not None:
                 y = y[test]
 
-        merged = aux.merge(scores.reset_index()[['address', 'score']], on='address', how='left')
+        scores = scores.reset_index()
+        merged = aux.merge(scores[['address', 'score']], on='address', how='left')
         merged.index = aux.index
+
+        missing = merged.score.isnull()
+        # if scores are null that means it's a "new" address
+        # so we use the geography score
+        # which is based on community area, ward, and census block
+        if missing.sum() > 0:
+            geography_columns = ['community_area_id', 'ward_id', 'census_block_id']
+            geography_scores = scores[scores.address.isnull()]
+            missing_scores = aux[missing].merge(geography_scores, on=geography_columns, how='left').score.values
+            merged.loc[missing, 'score'] = missing_scores
+
         if y is not None:
             merged['true'] = y
 
